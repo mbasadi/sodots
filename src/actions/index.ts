7aea3201a8151c1b9f3b37630dfbd6e0cb2fa93e
mbasadi <asadi.mohammadbagher@gmail.com>
2020-05-08 18:28:01 -0230

    devops

--- src/actions/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './sodos';
-export * from './types';
--- src/actions/types.ts
+++
@@ -1,8 +0,0 @@
-import { NewGameAction, SelectedItem } from './sodos';
-
-export enum ActionTypes {
-  newgame,
-  selecteditem,
-}
-
-export type Action = NewGameAction | SelectedItem;
