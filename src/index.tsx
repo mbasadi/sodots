827d83b9b1291abc4158aa46e47ee2a06f58294b
mbasadi <asadi.mohammadbagher@gmail.com>
2020-05-09 00:43:48 -0230

    devops1

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+// import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+// import thunk from 'redux-thunk';
 import App from './App';
 // import { reducers } from './reducers';
 
