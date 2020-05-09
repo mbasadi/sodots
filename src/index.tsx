7a85eb17521436dd42ae7c3c3dbb293c9ff96186
mbasadi <asadi.mohammadbagher@gmail.com>
2020-05-09 01:20:27 -0230

    devopsend

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import App from './App';
+import { Game } from './components/game';
 import { reducers } from './reducers';
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-  // <Provider store={store}>
-  <App />,
-  // </Provider>,
+  <Provider store={store}>
+    <Game />
+  </Provider>,
   document.querySelector('#root')
 );
