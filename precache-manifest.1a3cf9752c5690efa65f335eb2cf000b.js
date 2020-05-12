20cc2a99ab666222c90d3437fc8ea9ded250d7f2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-12 16:47:47 +0000

    Deploy mbasadi/sodots to github.com/mbasadi/sodots.git:gh-pages

---
+++ precache-manifest.1a3cf9752c5690efa65f335eb2cf000b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ad3e88f41310221eb54a8dc351763c",
+    "url": "/sodots/index.html"
+  },
+  {
+    "revision": "028a38fa6050a0328757",
+    "url": "/sodots/static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "2dae0a5f32c467bc431e",
+    "url": "/sodots/static/css/main.8f6a20db.chunk.css"
+  },
+  {
+    "revision": "028a38fa6050a0328757",
+    "url": "/sodots/static/js/2.d37b1251.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sodots/static/js/2.d37b1251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dae0a5f32c467bc431e",
+    "url": "/sodots/static/js/main.21ac32f8.chunk.js"
+  },
+  {
+    "revision": "d67f12c837dfaa9cf740",
+    "url": "/sodots/static/js/runtime-main.a92a47e3.js"
+  }
+]);
